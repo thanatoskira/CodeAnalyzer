9ec1338e26f23953168976ffbdf58763367bad52
thanat0s <8370120+thanatoskira@users.noreply.github.com>
2023-08-01 16:16:02 +0800

    fix getCallersFromClassNode bug and add getBTUpgradeCaller

--- src/main/java/org/observer/utils/SearchUtil.java
+++ src/main/java/org/observer/utils/SearchUtil.java
@@ -11,32 +11,55 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 
 public class SearchUtil {
-    public static Map<String, List> getBTCaller(String call) throws Exception {
+    public static Map<String, List> getBTCaller(String call) {
         Map<String, List> btTree = new ConcurrentHashMap<>();
-        getBTCallerInner(call, new CopyOnWriteArrayList<>(), btTree);
+        getBTCallerInner(call, new CopyOnWriteArrayList<>(), btTree, false);
+        return btTree;
+    }
+
+    public static Map<String, List> getBTUpgradeCaller(String call) {
+        Map<String, List> btTree = new ConcurrentHashMap<>();
+        getBTCallerInner(call, new CopyOnWriteArrayList<>(), btTree, true);
         return btTree;
     }
 
     /**
      * 递归搜索所有 call 的 caller
+     * 当 upgrade 至 父类/接口 方法时，添加 {x -> [super(x)]} 输出
      */
-    private static void getBTCallerInner(String call, List<String> group, Map<String, List> root) {
-        String cName = call.split("#")[0];
-        if (!group.contains(call)) {
-            group.add(call);
-            List elements = root.computeIfAbsent(call, k -> new ArrayList());
-            DependencyUtil.getAllDependencies(cName).stream().map(f -> {
+    private static void getBTCallerInner(String call, List<String> group, Map<String, List> root, boolean upgrade) {
+        String[] callItems = call.split("#");
+        Map<String, List> finalRoot = root;
+        if (upgrade) {
+            // upgrade 至 接口/父类 方法
+            try {
+                String finalName = HierarchyUtil.getIfaceMethodClzName(callItems[0], callItems[1], callItems[2]);
+                if (!finalName.equals(callItems[0])) {
+                    finalRoot = new ConcurrentHashMap<>();
+                    List<Map> upList = root.computeIfAbsent(call, k -> new ArrayList<Map>());
+                    upList.add(finalRoot);
+                    callItems[0] = finalName;
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        final String upgradeCall = String.join("#", callItems);
+        if (!group.contains(upgradeCall)) {
+            group.add(upgradeCall);
+            List<Map> elements = finalRoot.computeIfAbsent(upgradeCall, k -> new ArrayList<Map>());
+            DependencyUtil.getAllDependencies(callItems[0]).stream().map(f -> {
                 try {
-                    return getCallerFromFile(f, call);
+                    return getCallerFromFile(f, upgradeCall);
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
             }).forEach(pCallers -> pCallers.forEach(pCaller -> {
                 try {
                     Map<String, List> element = new ConcurrentHashMap<>();
-                    element.put(pCaller, new ArrayList());
+//                    element.put(pCaller, new ArrayList());
                     elements.add(element);
-                    getBTCallerInner(pCaller, group, element);
+                    getBTCallerInner(pCaller, group, element, upgrade);
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
@@ -78,7 +101,7 @@ public class SearchUtil {
     }
 
     private static List<String> getCallersFromClassNode(ClassNode classNode, String cName, String fName, String fDesc) {
-        return classNode.methods.stream().filter(methodNode -> !MethodUtil.isBlackMethod(methodNode) && !methodNode.name.equals(fName) && !methodNode.desc.equals(fDesc)).filter(methodNode -> {
+        return classNode.methods.stream().filter(methodNode -> !MethodUtil.isBlackMethod(methodNode) && (!methodNode.name.equals(fName) || !methodNode.desc.equals(fDesc))).filter(methodNode -> {
             try {
                 return MethodUtil.isCaller(methodNode, cName, fName, fDesc);
             } catch (Exception e) {
