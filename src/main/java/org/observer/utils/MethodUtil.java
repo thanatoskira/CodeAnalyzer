60caf29d51f92ee0185b4c9434c63665db6c2df4
thanat0s <8370120+thanatoskira@users.noreply.github.com>
2023-09-01 17:35:39 +0800

    fix matching issue in isCaller function

--- src/main/java/org/observer/utils/MethodUtil.java
+++ src/main/java/org/observer/utils/MethodUtil.java
@@ -19,9 +19,24 @@ public class MethodUtil {
             AbstractInsnNode inode = ins.get(i);
             if (inode.getType() == AbstractInsnNode.METHOD_INSN) {
                 MethodInsnNode miNode = (MethodInsnNode) inode;
-                if (miNode.name.equals(fName) && (fDesc.equals("null") || miNode.desc.equals(fDesc)) && HierarchyUtil.hasCommonAncestor(miNode.owner.replace("/", "."), cName)) {
-                    found = true;
-                    break;
+                if (miNode.name.equals(fName) && (fDesc.equals("null") || miNode.desc.equals(fDesc))) {
+                    try {
+                        /*
+                            使用 HierarchyUtil.hasCommonAncestor(miNode.owner.replace("/", "."), cName) 会导致两个不相关但存在相同父类或接口的类被关联在一起
+                            一个比较常见的场景就是 <init> 方法，如:
+                            搜索 cn.hutool.core.map.multi.CollectionValueMap#<init>()V 方法时会与 com.alibaba.fastjson.parser.JSONScanner#<init>()V 方法关联
+                            原因在于两者均实现了 java.lang.Cloneable 接口且 方法名 和 Desc 均一致
+                            解决方法为获取对应的接口方法类名，判断两者是否一致
+                         */
+                        String leftName = HierarchyUtil.getIfaceMethodClzName(miNode.owner.replace("/", "."), miNode.name, miNode.desc);
+                        String rightName = HierarchyUtil.getIfaceMethodClzName(cName, fName, fDesc);
+                        if (leftName.equals(rightName)) {
+                            found = true;
+                            break;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         }
@@ -29,7 +44,11 @@ public class MethodUtil {
     }
 
     public static boolean isBlackMethod(MethodNode methodNode) {
+        return isBlackMethod(methodNode.name);
+    }
+
+    public static boolean isBlackMethod(String fName) {
         final List<String> blackMethods = Arrays.asList("main", "<clinit>");
-        return blackMethods.contains(methodNode.name);
+        return blackMethods.contains(fName);
     }
 }
