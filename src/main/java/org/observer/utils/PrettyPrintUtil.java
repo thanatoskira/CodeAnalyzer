56d20fc8635a7f8c22ca57ed4b337ed410d076bb
thanat0s <8370120+thanatoskira@users.noreply.github.com>
2023-09-01 10:41:45 +0800

    add saveToFile method

--- src/main/java/org/observer/utils/PrettyPrintUtil.java
+++ src/main/java/org/observer/utils/PrettyPrintUtil.java
@@ -2,11 +2,20 @@ package org.observer.utils;
 
 import com.google.gson.Gson;
 
+import java.io.FileOutputStream;
 import java.util.Map;
 
 public class PrettyPrintUtil {
-    public static void print(Map map){
+    public static void prettyPrint(Map map) {
         System.out.println("====== Pretty Print ======");
         System.out.println(new Gson().toJson(map));
+    }
+
+    public static void saveToFile(Map map, String path) {
+        try (FileOutputStream outputStream = new FileOutputStream(path, true)) {
+            outputStream.write(new Gson().toJson(map).getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
